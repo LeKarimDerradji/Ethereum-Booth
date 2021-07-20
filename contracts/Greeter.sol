e122ec7f3af560a33dddee256fe6847ea959d7d7
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-07-21 00:52:40 +0200

    typo

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.6;
 
 contract EthereumBooth {
 
-    enum Status {live, expired, banned}
+    enum Status {live, expiredgi}
     
     struct User {
         uint256 userId;
         uint256 registrationDate;
         uint256 credit;
         uint64 numbers;
+        Status status;
     }
 
     constructor() {
